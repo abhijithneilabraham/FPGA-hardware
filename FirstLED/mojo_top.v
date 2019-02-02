45df2049a53a61018d4c46da36f7464d26569a57
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-02-02 22:55:10 +0530

    Create mojo_top.v

---
+++ FirstLED/mojo_top.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:29:58 02/02/2019 
+// Design Name: 
+// Module Name:    mojo_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mojo_top(
+    input RESET_N,
+    output LED
+    );
+
+
+endmodule
